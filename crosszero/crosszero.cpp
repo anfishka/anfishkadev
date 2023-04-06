9e0bfd8fbb3e4db4e80d012fe8141a304515e580
Hanna Borodina <venera131916@gmail.com>
2023-04-06 12:17:16 +0300

    add x `s and o`s steps

--- crosszero/crosszero.cpp
+++ crosszero/crosszero.cpp
@@ -18,6 +18,23 @@ int main()
 		cout << endl;
 		cout << "\x1b[33m----------\033[0m" << endl;
 	}
+
+
+	for (int i = 0; i < 9; i++)
+	{
+		char xo = (i % 2 == 0 ? 'X' : 'O');
+		cout << "\x1b[33mNow is your turn : \033[0m" << xo << endl;
+		int row, col;
+		do {
+			cout << "\x1b[33mEnter num of row (0-2):\033[0m";
+			cin >> row;
+			cout << "\x1b[33mEnter num of col (0-2):\033[0m";
+			cin >> col;
+		} while (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col].spot != ' ');
+
+		board[row][col].spot = xo;
+
+	}
 	return 0;
 }
 
